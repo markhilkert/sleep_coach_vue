7e43713f49533ffe940b7d1e146f7038ba53d5d9
markhilkert <mark.hilkert@gmail.com>
2019-03-12 14:53:05 -0500

    created login, logout, installed bootstrap, created a home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Sleep Coach</h1>
+      <div>
+        <button class="btn btn-primary" v-on:click="startSleep()"> Begin Sleeping </button>
+      </div>
+        
+      <div v-if="this.sleeping">
+        <h2>You are currently sleeping.</h2>
+      </div>
+      
+      <div>
+        <button class="btn btn-success" v-on:click="endSleepGood()"> I slept well </button>
+      </div>
+
+      <div>
+        <button class="btn btn-danger" v-on:click="endSleepGood()"> I slept poorly </button>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      errors: [],
+      sleeping: false
+    };
+  },
+  methods: {
+    startSleep: function() {
+      axios.post("/api/sleeps/start")
+      this.sleeping = true
+      console.log(sleeping)
+      .then(response => {
+        // this.$router.push(somewhere??? + response.data.id);
+      }).catch(error => {
+        this.errors = error.response.data.errors;
+      });
+    },
+    endSleepGood: function() {
+      this.sleeping = false
+      axios.post("/api/sleeps/end_good")
+      .then(response => {
+        // this.$router.push(somewhere??? + response.data.id);
+      }).catch(error => {
+        this.errors = error.response.data.errors;
+      });
+    },
+    endSleepBad: function() {
+      this.sleeping = false
+      axios.post("/api/sleeps/end_bad")
+      .then(response => {
+        // this.$router.push(somewhere??? + response.data.id);
+      }).catch(error => {
+        this.errors = error.response.data.errors;
+      });
+    }
   }
-}
+};
 </script>
