0893a1ddd7d6d96233b20ecf8456b8b532da9ea9
markhilkert <mark.hilkert@gmail.com>
2019-03-14 14:55:38 -0500

    initial sleep information page

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,26 +1,11 @@
 <template>
   <div class="home">
     <div class="container">
-      <h1>Sleep Coach</h1>
+      <h1>Your Sleep Stats</h1>
       <div>
-        <button class="btn btn-primary" v-on:click="startSleep()"> Begin Sleeping </button>
-      </div>
-        
-      <div v-if="this.sleeping">
-        <h2>You are now sleeping.</h2>
-      </div>
-      
-      <div>
-        <button class="btn btn-success" v-on:click="endSleepGood()"> I slept well </button>
+        <h2>Last night, you spent </h2>
       </div>
 
-      <div>
-        <button class="btn btn-danger" v-on:click="endSleepGood()"> I slept poorly </button>
-      </div>
-
-      <div>
-        <router-link v-bind:to="'/statistics/'"> See my Sleep Stats </router-link>
-      </div>
 
     </div>
   </div>
@@ -32,39 +17,18 @@ import axios from "axios";
 export default {
   data: function() {
     return {
+      sleeps: [],
       errors: [],
       sleeping: false
     };
   },
-  methods: {
-    startSleep: function() {
-      axios.post("/api/sleeps/start")
-      this.sleeping = true
-      console.log(sleeping)
+  created: function() {
+    axios.get("/api/sleeps/")
       .then(response => {
-        // this.$router.push(somewhere??? + response.data.id);
-      }).catch(error => {
-        this.errors = error.response.data.errors;
-      });
-    },
-    endSleepGood: function() {
-      this.sleeping = false
-      axios.post("/api/sleeps/end_good")
-      .then(response => {
-        // this.$router.push(somewhere??? + response.data.id);
-      }).catch(error => {
-        this.errors = error.response.data.errors;
-      });
-    },
-    endSleepBad: function() {
-      this.sleeping = false
-      axios.post("/api/sleeps/end_bad")
-      .then(response => {
-        // this.$router.push(somewhere??? + response.data.id);
-      }).catch(error => {
-        this.errors = error.response.data.errors;
-      });
-    }
-  }
+        this.sleeps = response.data;
+        console.log(this.sleeps)
+      })
+  },
+  methods: {}
 };
 </script>
