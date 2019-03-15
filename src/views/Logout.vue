b7a49ee02462cd621cd1aaa34cb57ce705ef01db
markhilkert <mark.hilkert@gmail.com>
2019-03-15 16:25:28 -0500

    updated navbar

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -13,6 +13,7 @@ export default {
   created: function() {
     delete axios.defaults.headers.common["Authorization"];
     localStorage.removeItem("jwt");
+    localStorage.removeItem("loggedIn");
     this.$router.push("/login");
   }
 };
