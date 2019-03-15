34af1962d1a41be81565ecd6c353b32b1a3dff5e
markhilkert <mark.hilkert@gmail.com>
2019-03-15 17:49:10 -0500

    added a new background image, put sleep form into bootstrap template, more to go with that one

--- src/views/SleepInformation.vue
+++ src/views/SleepInformation.vue
@@ -1,60 +1,131 @@
 <template>
   <div class="sleep-information">
 
+    <div class='container'>
+    <!-- How do work -->
+    <section class="section services-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="section-title mx-auto">
+                        <h3 class="mb-4">How did you sleep last night?</h3>
+                        <p class="text-muted"> [maybe add txt] </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <div class="service-boxed bg-white p-4">
+                    <i class="mbri-globe service-icon font-weight-bold"></i>
+                    <div class="service-body pt-3">
+                        <h5 class=""> <span class="">01.</span> General </h5>
+
+                        <form v-on:submit.prevent="submit()">
+
+                          <div class="form-group">
+                            <label>Did you sleep well last night? </label>
+                            <input class='form-control' type='text' v-model="sleep.good_sleep" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Did you take a hot shower or bath before bed? </label>
+                            <input class='form-control' type='text' v-model="sleep.bath_before_bed" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label> Was your bedroom completely dark? </label>
+                            <input class='form-control' type='text' v-model="sleep.dark_room" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Did you feel cool (temperature) while you slept? </label>
+                            <input class='form-control' type='text' v-model="sleep.cool_room" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Were any clock faces visible from your bed? </label>
+                            <input class='form-control' type='text' v-model="sleep.clock_visible" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Did you have any electronics with screens in your bedroom? </label>
+                            <input class='form-control' type='text' v-model="sleep.electronics_in_room" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label>At any point, did you lie in bed awake longer than 20 minutes trying to fall asleep? </label>
+                            <input class='form-control' type='text' v-model="sleep.lie_in_bed" placeholder="">
+                          </div>
+
+                          <div class="form-group">
+                            <label>What was the temperature of your room, in Fahrenheit? </label>
+                            <input class='form-control' type='text' v-model="sleep.room_temperature" placeholder="">
+                          </div>
+                          
+                          <div class="new-button">
+                            <input type="submit" value="Add Sleep Data" class="btn btn-primary">
+                          </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+
+                <div class="col-lg-4">
+                    <div class="service-boxed bg-white p-4">
+                        <i class="mbri-globe service-icon font-weight-bold"></i>
+                        <div class="service-body pt-3">
+                            <h5 class=""> <span class="">01.</span> Pre-Planning</h5>
+                            <form v-on:submit.prevent="submit()">
+
+                              <div class="form-group">
+                                <label>Did you sleep well last night? </label>
+                                <input class='form-control' type='text' v-model="sleep.good_sleep" placeholder="">
+                              </div>
+
+                              <div class="new-button">
+                                <input type="submit" value="Add Sleep Data" class="btn btn-primary">
+                              </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="service-boxed bg-white p-4">
+                        <i class="mbri-calendar service-icon font-weight-bold"></i>
+                        <div class="service-body pt-3">
+                            <h5 class=""> <span class="">02.</span> Perfect Design</h5>
+                            <p class="text-muted">
+                                Lorem ipsum dolor sit amet consectetuer adipiscing elit, Aenean commodo ligula eget dolor Aenean elight massa.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="service-boxed bg-white p-4">
+                        <i class="mbri-layers service-icon font-weight-bold"></i>
+                        <div class="service-body pt-3">
+                            <h5> <span class="">03.</span>Communication</h5>
+                            <p class="text-muted">
+                                Lorem ipsum dolor sit amet consectetuer adipiscing elit, Aenean/ commodo ligula eget dolor Aenean elight massa.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  </div>
+    <!-- How do work end  -->
+
     <ul>
       <li v-for="error in errors"> {{ error }} </li>
     </ul>
 
-    <div class='container'>
-      <h2>How did you sleep last night?</h2>
-      <form v-on:submit.prevent="submit()">
-        <!-- Make this accordion style, so it's actually a bunch of forms, not one giant form -->
-        <!-- Don't actually do models before we pick out themes, it might conflict. -->
-        <div class="form-group">
-          <label>Did you sleep well last night? </label>
-          <input class='form-control' type='text' v-model="sleep.good_sleep" placeholder="">
-        </div>
+       
 
-        <div class="form-group">
-          <label>Did you take a hot shower or bath before bed? </label>
-          <input class='form-control' type='text' v-model="sleep.bath_before_bed" placeholder="">
-        </div>
-
-        <div class="form-group">
-          <label> Was your bedroom completely dark? </label>
-          <input class='form-control' type='text' v-model="sleep.dark_room" placeholder="">
-        </div>
-
-        <div class="form-group">
-          <label>Did you feel cool (temperature) while you slept? </label>
-          <input class='form-control' type='text' v-model="sleep.cool_room" placeholder="">
-        </div>
-
-        <div class="form-group">
-          <label>Were any clock faces visible from your bed? </label>
-          <input class='form-control' type='text' v-model="sleep.clock_visible" placeholder="">
-        </div>
-
-        <div class="form-group">
-          <label>Did you have any electronics with screens in your bedroom? </label>
-          <input class='form-control' type='text' v-model="sleep.electronics_in_room" placeholder="">
-        </div>
-
-        <div class="form-group">
-          <label>At any point, did you lie in bed awake longer than 20 minutes trying to fall asleep? </label>
-          <input class='form-control' type='text' v-model="sleep.lie_in_bed" placeholder="">
-        </div>
-
-        <div class="form-group">
-          <label>What was the temperature of your room, in Fahrenheit? </label>
-          <input class='form-control' type='text' v-model="sleep.room_temperature" placeholder="">
-        </div>
-        
-        <div class="new-button">
-          <input type="submit" value="Add Sleep Data" class="btn btn-primary">
-        </div>
-      </form>
-    </div>
   </div>
 </template>
 
@@ -84,7 +155,18 @@ export default {
               electronics_in_room: "",
               lie_in_bed: "",
               room_temperature: "",
-              user_id: ""
+              user_id: "",
+              
+              alcohol: {
+                        amount: "",
+                        time: "",
+                        increased_impact: ""
+                        },
+
+              exercise: {
+                        duration: "",
+                        time: ""
+                        }
               },
       errors: []
     };
