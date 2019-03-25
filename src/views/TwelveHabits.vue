821bb3c190a19b16e229ec80ec3e63a157629378
markhilkert <mark.hilkert@gmail.com>
2019-03-25 09:16:12 -0500

    added twelve habits page in format of FAQ

---
+++ src/views/TwelveHabits.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="real-home">
+    <div class="container">
+      
+      <!-- home start -->
+      <section class="heading-half bg-sub-page-home">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12 page-next-level text-center">
+                      <h4 class="pt-3">Frequently Asked Questions</h4>
+                      <div class="page-next text-light"> <a href="index.html">Home</a>/<span>Faq</span> </div>
+                  </div>                    
+              </div>
+          </div>
+      </section>
+      <!-- home end -->   
+
+      <!-- Faq -->
+      <section class="section">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-6">
+                      <div class="custom-faq mt-3">
+
+                          <h6 class="mb-4"> <i class="mdi mdi-help-circle"></i> &nbsp; How do I become an designer </h6>
+                          <p>One disadvantage of Lorum Ipsum is that in Latin certain letters appear more frequently than others - which creates a distinct visual impression. Moreover, in Latin only words at the beginning of sentences are capitalized; this means that Lorem Ipsum cannot accurately represent.</p>
+
+                          <div class="underline"></div>
+
+                         <h6 class="mb-4"> <i class="mdi mdi-help-circle"></i> &nbsp;can I make How much money  </h6>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, fugiat iste nisi tempore nesciunt nemo fuga? Nesciunt, delectus laboriosam nisi repudiandae nam fuga saepe animi recusandae.</p>
+
+                          <div class="underline"></div>
+
+                          <h6 class="mb-4"> <i class="mdi mdi-help-circle"></i>&nbsp; What is a Marketplace </h6>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quisquam atque vero delectus corrupti! Quo, maiores, dolorem, hic commodi nulla ratione accusamus doloribus fuga magnam id temporibus dignissimos deleniti quidem ipsam corporis sapiente nam expedita saepe quas Vero, assumenda.</p>
+
+                      </div>
+                  </div>
+                  <div class="col-lg-6">
+                      <div class="custom-faq mt-3">
+
+                          <h6 class="mb-4"> <i class="mdi mdi-help-circle"></i>&nbsp; How do I become an author </h6>
+                          <p>One disadvantage of Lorum Ipsum is that in Latin certain letters appear more frequently than others - which creates a distinct visual impression. Moreover, in Latin only words at the beginning of sentences are capitalized; this means that Lorem Ipsum cannot accurately represent.</p>
+
+                          <div class="underline"></div>
+
+                         <h6 class="mb-4"> <i class="mdi mdi-help-circle"></i> How can i get a money </h6>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, fugiat iste nisi tempore nesciunt nemo fuga? Nesciunt, delectus laboriosam nisi repudiandae nam fuga saepe animi recusandae.</p>
+
+                          <div class="underline"></div>
+
+                          <h6 class="mb-4"> <i class="mdi mdi-help-circle"></i> &nbsp; How do I become an designer </h6>
+                          <p>One disadvantage of Lorum Ipsum is that in Latin certain letters appear more frequently than others - which creates a distinct visual impression. Moreover, in Latin only words at the beginning of sentences are capitalized; this means that Lorem Ipsum cannot accurately represent.</p>
+
+                      </div>
+                  </div>  
+              </div>
+          </div>
+      </section>
+      <!-- Faq end -->
+
+
+
+      
+
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      errors: [],
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
